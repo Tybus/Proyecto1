9ef3bd4795c2285264a2e9ffda235e6c9556a822
Mario Castresana Avendaño <Mario Castresana Avendao>
2016-08-31 11:12:51 -0600

    Se incluye el arreglo para Read After Write
    Cambios:
        /ReadWrite/MiniAlu.v

---
+++ ReadWrite/Module_ROM.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+`include "Defintions.v"
+
+`define LOOP1 8'd8
+`define LOOP2 8'd5
+module ROM
+(
+	input  wire[15:0]  		iAddress,
+	output reg [27:0] 		oInstruction
+);	
+always @ ( iAddress )
+begin
+	case (iAddress)
+	0: oInstruction = { `STO ,`R1,16'd1};
+	1: oInstruction = { `STO ,`R1,16'd1};
+	2: oInstruction = { `STO ,`R2,16'd1};
+	3: oInstruction = { `NOP, 24'd4000};
+	4: oInstruction = { `ADD, `R1, `R1,`R2}; 
+	5: oInstruction = { `JMP, 8'd5, 16'b0};
+	default:
+		oInstruction = { `LED ,  24'b10101010 };		//NOP
+	endcase	
+end
+	
+endmodule
